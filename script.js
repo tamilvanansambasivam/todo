797aef717c68d1aca3b6d1a70152d94606f67ca2
tamilvanan <tamilvanansambasivam@gmail.com>
2024-02-18 08:53:55 +0530

    Formatted script.js

--- script.js
+++ script.js
@@ -1,54 +1,48 @@
-const inputBox = document.getElementById('input-box');
-const listContainer = document.getElementById('list-container');
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
 
-function addTask(){
-    if(inputBox.value === ''){
-        alert("You must write something!");
-    }
-    else{
-        let li = document.createElement("li");
-        li.innerHTML = inputBox.value;
-        listContainer.appendChild(li); 
-        let span = document.createElement("span");
-        span.innerHTML = "\u00d7";
-        li.appendChild(span)
-        
-    }
-    inputBox.value = "";
+function addTask() {
+  if (inputBox.value === "") {
+    alert("You must write something!");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+  }
+  inputBox.value = "";
 
-    saveData();
+  saveData();
 }
 
-
-
-listContainer.addEventListener("click", function(e){
-    if(e.target.tagName === "LI"){
-        e.target.classList.toggle("checked");
-        saveData();
-
+listContainer.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+      saveData();
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+      saveData();
     }
-    else if(e.target.tagName === "SPAN"){
-        e.target.parentElement.remove();
-        saveData();
-    }
-},false);
+  },
+  false
+);
 
-
-
-
-function saveData(){
-    localStorage.setItem("data", listContainer.innerHTML);
+function saveData() {
+  localStorage.setItem("data", listContainer.innerHTML);
 }
 
-function showTask(){
-    listContainer.innerHTML = localStorage.getItem("data");
-
+function showTask() {
+  listContainer.innerHTML = localStorage.getItem("data");
 }
 
 showTask();
 
-inputBox.addEventListener("keydown", function(e) {
-    if (e.key === "Enter") {
-      addTask();
-    }
-  });
+inputBox.addEventListener("keydown", function (e) {
+  if (e.key === "Enter") {
+    addTask();
+  }
+});
